15f0c423e9c05b18d731e3b0f0bec96626c58ff2
andrewjsauer <andrewjohnsauer@gmail.com>
2016-06-04 15:56:29 -0700

    Changed SDK version number

--- app/src/main/java/andrewjsauer/ruler/MainActivity.java
+++ app/src/main/java/andrewjsauer/ruler/MainActivity.java
@@ -3,8 +3,6 @@ package andrewjsauer.ruler;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
